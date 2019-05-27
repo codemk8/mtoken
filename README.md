697aae8b26c5c28e263351b75b3b6676259d8980
Code M <codemk8@gmail.com>
2019-05-27 10:16:04 -0400

    Link to muser microservice

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # mtoken
+
+## Prerequisite
+
+Run the other microservice [muser](https://github.com/codemk8/muser). Assume it is run in the same `localhost`.
+
+## Build and run mtoken service
+
+```bash
+make
+./bin/mtoken
+```
+
+## Test
+
+```
+$ curl -X POST --user test_user:secret http://127.0.0.1:8001/v1/token/login
+```
--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"flag"
 	"fmt"
 	"log"
@@ -11,11 +12,38 @@ import (
 )
 
 var ip = flag.String("addr", "127.0.0.1:8001", "Serving host and port")
-var user_server = flag.String("user_service", "127.0.0.1:", "User microservice endpoint")
+var userAuthService = flag.String("user_service", "http://localhost:8000/v1/user/auth", "User microservice downstream endpoint")
 var apiRoot = flag.String("api_root", "/v1", "api root path")
 
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+func redirectPolicyFunc(req *http.Request, via []*http.Request) error {
+	req.Header.Add("Authorization", "Basic "+basicAuth("username1", "password123"))
+	return nil
+}
+
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Inside register handler")
+	username, passwd, authOK := r.BasicAuth()
+	if authOK == false {
+		http.Error(w, "Not authorized", http.StatusUnauthorized)
+		return
+	}
+	client := &http.Client{CheckRedirect: redirectPolicyFunc}
+	req, err := http.NewRequest("GET", *userAuthService, nil)
+	req.Header.Add("Authorization", "Basic "+basicAuth(username, passwd))
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Internal error %v", err)
+		http.Error(w, "Internal Error", http.StatusInternalServerError)
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		http.Error(w, "Unauthorized request", http.StatusUnauthorized)
+		return
+	}
 	return
 }
 
@@ -23,7 +51,7 @@ func main() {
 	flag.Parse()
 
 	r := mux.NewRouter()
-	r.HandleFunc(*apiRoot+"/user/login", loginHandler).Methods("GET")
+	r.HandleFunc(*apiRoot+"/token/login", loginHandler).Methods("POST")
 	srv := &http.Server{
 		Handler: r,
 		Addr:    *ip,
@@ -31,6 +59,6 @@ func main() {
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
-	fmt.Printf("Running server on %s/user/login.\n", *ip)
+	fmt.Printf("Running server on %s/token/login.\n", *ip)
 	log.Fatal(srv.ListenAndServe())
 }
