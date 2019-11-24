2b4b035b0efd480aba334f0be5bee1de78ea64dd
CodeM <codemk8@gmail.com>
2019-11-24 08:38:45 -0500

    Issue request accepts token header

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"encoding/base64"
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
 	"strings"
 	"time"
 
 	"github.com/codemk8/mtoken/pkg/token"
+	"github.com/golang/glog"
 	"github.com/gorilla/mux"
 	"gopkg.in/square/go-jose.v2/jwt"
 )
@@ -17,6 +17,7 @@ import (
 var ip = flag.String("addr", "127.0.0.1:8001", "Serving host and port")
 var userAuthService = flag.String("user_service", "http://localhost:8000/v1/user/auth", "User microservice downstream endpoint")
 var apiRoot = flag.String("api_root", "/v1", "api root path")
+var appName = flag.String("app_name", "yourapp", "app name")
 var privKeyFile = flag.String("priv_key", "", "private key file path")
 var pubKeyFile = flag.String("pub_key", "", "public key file path")
 var signer token.Signer
@@ -32,18 +33,59 @@ func redirectPolicyFunc(req *http.Request, via []*http.Request) error {
 	return nil
 }
 
+func genToken(username string) (*string, error) {
+	expiry := jwt.NewNumericDate(time.Now().Add(time.Minute * time.Duration(10)))
+	claims := jwt.Claims{Issuer: *appName,
+		Subject: username,
+		Expiry:  expiry}
+	jwt, err := signer.Sign(&claims)
+	if err != nil {
+		glog.Errorf("Internal error %v", err)
+	}
+	return jwt, err
+}
+
 func issueHandler(w http.ResponseWriter, r *http.Request) {
 	username, passwd, authOK := r.BasicAuth()
 	if authOK == false {
+		authorizationHeader := r.Header.Get("Authorization")
+		if authorizationHeader != "" {
+			bearerToken := strings.Split(authorizationHeader, " ")
+			if len(bearerToken) == 2 {
+				claims, err := verifier.Verify(&bearerToken[1])
+				if err != nil {
+					glog.Errorf("Invalid token %v", err)
+					http.Error(w, "Invalid token", http.StatusUnauthorized)
+					return
+				}
+				if (int64)(*claims.Expiry) < time.Now().Unix() {
+					glog.Errorf("Token expired")
+					http.Error(w, "Token expired", http.StatusUnauthorized)
+					return
+				}
+				jwt, err := genToken(claims.Subject)
+				if err != nil {
+					glog.Errorf("Internal error %v", err)
+					http.Error(w, "Internal Error", http.StatusInternalServerError)
+					return
+				}
+				glog.Infof("generated jwt %s.", *jwt)
+				w.Write([]byte(*jwt))
+				return
+			}
+			http.Error(w, "Not authorized", http.StatusUnauthorized)
+			return
+		}
 		http.Error(w, "Not authorized", http.StatusUnauthorized)
 		return
 	}
+
 	client := &http.Client{CheckRedirect: redirectPolicyFunc}
 	req, err := http.NewRequest("GET", *userAuthService, nil)
 	req.Header.Add("Authorization", "Basic "+basicAuth(username, passwd))
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("Internal error %v", err)
+		glog.Errorf("Internal error %v", err)
 		http.Error(w, "Internal Error", http.StatusInternalServerError)
 		return
 	}
@@ -52,13 +94,9 @@ func issueHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	// Generate a token
-	expiry := jwt.NewNumericDate(time.Now().AddDate(0, 1, 0)) // add a month
-	claims := jwt.Claims{Issuer: username,
-		Subject: "yourapp",
-		Expiry:  expiry}
-	jwt, err := signer.Sign(&claims)
+	// expiry := jwt.NewNumericDate(time.Now().AddDate(0, 1, 0)) // add a month
+	jwt, err := genToken(username)
 	if err != nil {
-		fmt.Printf("Internal error %v", err)
 		http.Error(w, "Internal Error", http.StatusInternalServerError)
 		return
 	}
@@ -67,8 +105,8 @@ func issueHandler(w http.ResponseWriter, r *http.Request) {
 	//	Value:   tokenString,
 	//	Expires: expirationTime,
 	//})
+	glog.Infof("generated jwt %s.", *jwt)
 	w.Write([]byte(*jwt))
-	return
 }
 
 func authHandler(w http.ResponseWriter, r *http.Request) {
@@ -78,12 +116,12 @@ func authHandler(w http.ResponseWriter, r *http.Request) {
 		if len(bearerToken) == 2 {
 			claims, err := verifier.Verify(&bearerToken[1])
 			if err != nil {
-				fmt.Printf("Invalid token %v", err)
+				glog.Errorf("Invalid token %v", err)
 				http.Error(w, "Invalid token", http.StatusUnauthorized)
 				return
 			}
 			if (int64)(*claims.Expiry) < time.Now().Unix() {
-				fmt.Printf("Token expired")
+				glog.Errorf("Token expired")
 				http.Error(w, "Token expired", http.StatusUnauthorized)
 			}
 			return
@@ -108,7 +146,7 @@ func main() {
 	}
 
 	r := mux.NewRouter()
-	r.HandleFunc(*apiRoot+"/token/issue", issueHandler).Methods("POST")
+	r.HandleFunc(*apiRoot+"/token/issue", issueHandler).Methods("GET")
 	r.HandleFunc(*apiRoot+"/token/auth", authHandler).Methods("GET")
 
 	srv := &http.Server{
@@ -118,6 +156,6 @@ func main() {
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
-	fmt.Printf("Running server on %s/token/login.\n", *ip)
+	glog.Errorf("Running server on %s%s/token/issue(auth).\n", *ip, *apiRoot)
 	log.Fatal(srv.ListenAndServe())
 }
